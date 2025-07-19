b9f662b1ed5d85c9e9976224f36ffc96ca0fa00a
varshinicb1 <cbvarshini1@gmail.com>
2025-07-20 00:54:40 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='edgehax_tester',
+    version='1.0',
+    description='Edgehax Board Tester Software',
+    author='Varshini CB',
+    packages=[],
+    install_requires=['PyQt5==5.15.10', 'pyserial', 'pyqtgraph'],
+)
